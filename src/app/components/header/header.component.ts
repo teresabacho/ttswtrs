eee2efb9cd659d06aa3c0575c17a22ec776a5517
teresabacho <terkabacho@gmail.com>
2021-08-19 18:19:44 +0300

    added burgermenu1.1

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public opened: boolean = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+
+  openBurger(): void {
+    if(!this.opened){
+      document.querySelector(".burgerMenuBottom")?.classList.remove("displayNone");
+      document.querySelector(".burgerMenuBottom")?.classList.add("displayFlex");
+      this.opened = true;
+    } else {
+      document.querySelector(".burgerMenuBottom")?.classList.remove("displayFlex");
+      document.querySelector(".burgerMenuBottom")?.classList.add("displayNone");
+      this.opened = false;
+    }
+  }
+
 
 }
