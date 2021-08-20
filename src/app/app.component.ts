84bcc30cd4d39c7f8036dddb8918ddcc41da2e17
teresabacho <terkabacho@gmail.com>
2021-08-20 12:26:44 +0300

    fixed header, need add animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ttswtrs';
+
+  closeCollections(): void {
+    document.querySelector(".fallContainer")?.classList.add("displayNone");
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  public opened: boolean = false;
+  private opened: boolean = false;
 
   constructor() {
   }
@@ -15,16 +15,59 @@ export class HeaderComponent implements OnInit {
   }
 
   openBurger(): void {
-    if(!this.opened){
+    if (!this.opened) {
       document.querySelector(".burgerMenuBottom")?.classList.remove("displayNone");
       document.querySelector(".burgerMenuBottom")?.classList.add("displayFlex");
+
+      document.querySelector(".burgerMenuButton")?.classList.remove("burgerMenuButtonClosed");
+      document.querySelector(".burgerMenuButton")?.classList.add("burgerMenuButtonOpened");
       this.opened = true;
     } else {
       document.querySelector(".burgerMenuBottom")?.classList.remove("displayFlex");
       document.querySelector(".burgerMenuBottom")?.classList.add("displayNone");
+
+      document.querySelector(".burgerMenuButton")?.classList.remove("burgerMenuButtonOpened");
+      document.querySelector(".burgerMenuButton")?.classList.add("burgerMenuButtonClosed");
       this.opened = false;
     }
   }
+
+  closeBurger(): void {
+    document.querySelector(".burgerMenuBottom")?.classList.remove("displayFlex");
+    document.querySelector(".burgerMenuBottom")?.classList.add("displayNone");
+
+    document.querySelector(".burgerMenuButton")?.classList.remove("burgerMenuButtonOpened");
+    document.querySelector(".burgerMenuButton")?.classList.add("burgerMenuButtonClosed");
+    this.opened = false;
+  }
+
+  openCollections(): void {
+    document.querySelector(".fallContainer")?.classList.remove("displayNone");
+
+    document.querySelector(".fallContainerCollCat")?.classList.remove("displayNone");
+    document.querySelector(".fallContainerCollCat")?.classList.add("displayFlex");
+    document.querySelector(".exploreContainer")?.classList.add("displayNone");
+    document.querySelector(".exploreContainer")?.classList.remove("displayFlex");
+  }
+
+  closeCollections(): void {
+    document.querySelector(".fallContainer")?.classList.add("displayNone");
+  }
+
+
+
+  openExplore(): void {
+    document.querySelector(".fallContainer")?.classList.remove("displayNone");
+    document.querySelector(".fallContainerCollCat")?.classList.add("displayNone");
+    document.querySelector(".fallContainerCollCat")?.classList.remove("displayFlex");
+    document.querySelector(".exploreContainer")?.classList.remove("displayNone");
+    document.querySelector(".exploreContainer")?.classList.add("displayFlex");
+  }
+
+  closeExplore(): void {
+    document.querySelector(".fallContainer")?.classList.add("displayNone");
+  }
+
 
 
 }
